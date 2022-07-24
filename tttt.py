0466c8b73fd82bd6d7fdf3bce8242e08eff91681
dmkt21 <vova.makieievwork@gmail.com>
2022-07-24 20:11:15 +0200

    Submit HW16

---
+++ tttt.py
@@ -0,0 +1,50 @@
+
+class TVController():
+
+    def __init__(self, list_of_channels):
+        self.list_of_channels = list_of_channels
+        self.default_channel = 0
+
+    def first_channel(self):
+        return self.list_of_channels[0]
+
+    def last_channel(self):
+        return self.list_of_channels[-1]
+
+    def turn_channel(self, channel_number):
+        self.default_channel = channel_number - 1
+        return self.current_channel()
+
+    def previous_next(self, delta):
+        self.default_channel = (self.default_channel + delta) % \
+                               len(self.list_of_channels)
+        return self.current_channel()
+
+    def next_channel(self):
+        return self.previous_next(1)
+
+    def previous_channel(self):
+        return self.previous_next(-1)
+
+    def current_channel(self):
+        return self.list_of_channels[self.default_channel]
+
+    def is_exist(self, channel_name):
+        if isinstance(channel_name, int):
+            x = 0 <= channel_name < len(self.list_of_channels)
+        else:
+            x = channel_name in self.list_of_channels
+        return ('No', 'Yes')[x]
+
+
+CHANNELS = ["BBC", "Discovery", "TV1000"]
+
+controller = TVController(CHANNELS)
+print(controller.first_channel() == 'BBC')
+print(controller.last_channel() == 'TV1000')
+print(controller.turn_channel(1) == 'BBC')
+print(controller.next_channel() == 'Discovery')
+print(controller.previous_channel() == 'BBC')
+print(controller.current_channel() == 'BBC')
+print(controller.is_exist(4) == "No")
+print(controller.is_exist("BBC") == "Yes")
